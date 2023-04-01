5a753e259d54c3c766c2cf6ad8726ccdffd22486
Carter Brown <108925950+Cartman3@users.noreply.github.com>
2023-04-01 12:35:49 -0600

    Add files via upload

---
+++ Set_try_yourself_problem.py
@@ -0,0 +1,53 @@
+primary_colors = set()
+nonpri_colors = set()
+
+def intersection_of_sets(primary_colors, nonpri_colors):
+    # Code goes here for Problem 2
+    pass
+
+
+###################
+    # Problem 1 #
+###################
+
+# You can mess with this to get familiar with add and remove functions
+# Add colors to primary color set and non primary color set.
+# The add function also takes in strings you'll need to make multiple statements or use ,
+
+
+print()
+print("----------Problem 1----------")
+print()
+
+# Code goes here
+
+print(primary_colors) # prints the colors in the set
+print(nonpri_colors)  # prints the colors in the set
+pass
+
+
+####################
+# Try using duplicates!
+# Using the len() you can see how many are in each set
+# You can also try removing the elements
+####################
+
+###################
+    # Problem 2 #
+###################
+
+# This problem will be working with the intersection fuction
+###### Make sure to add the same color to each set like white to both sets #####
+
+
+print()
+print('----------Problem 2----------')
+print()
+
+# Code goes here
+# Make sure to add White and Black to each set
+
+print(primary_colors) # prints the colors in the set
+print(nonpri_colors)  # prints the colors in the set
+
+print(intersection_of_sets(primary_colors, nonpri_colors)) # 'White, Black'
